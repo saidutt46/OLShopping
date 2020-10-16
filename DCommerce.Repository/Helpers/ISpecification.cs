1c11feba31f8b8a8204cd527851f3294e3e6e554
Ganduri Venkata Sai Dutt <duttganduri46@gmail.com>
2020-10-16 19:43:14 -0400

    more updates for login

--- DCommerce.Repository/Helpers/ISpecification.cs
+++ DCommerce.Repository/Helpers/ISpecification.cs
@@ -9,5 +9,8 @@ namespace DCommerce.Repository.Helpers
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
         List<string> IncludeStrings { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+        Expression<Func<T, object>> GroupBy { get; }
     }
 }
